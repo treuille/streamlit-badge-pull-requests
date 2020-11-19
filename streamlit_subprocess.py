98b12488bbba612bcd9e081a0d67946d6c0406e9
Adrien Treuille <adrien.g.treuille@gmail.com>
2020-11-19 03:43:36 -0800

    Now returning return_code from streamlit_subprocess

--- streamlit_subprocess.py
+++ streamlit_subprocess.py
@@ -7,10 +7,13 @@ import subprocess
 def run(args):
     """Runs a commmand on the command line and outputs the result to Streamlit."""
     # Print out the command.
-    st.write(f"**`{' '.join(args)}`**")
+    cmd = ' '.join(args) 
+    st.write(f"**`{cmd}`**")
     
     # Start the subprocess.
-    proc = subprocess.Popen(args, stdout=subprocess.PIPE, encoding='utf-8')
+    proc = subprocess.Popen(args,
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+        encoding='utf-8', universal_newlines=True, bufsize=1)
     
     # Run the subprocess and write the output to Streamlit.
     output = ''
@@ -18,4 +21,7 @@ def run(args):
     while chunk := proc.stdout.readline():
         output += chunk
         output_block.code(output, language='sh')
-    
+    return_code = proc.wait(timeout=1.0)
+    if return_code is None:
+        raise RuntimeError(f'Cmd "{cmd}" taking too long to return.')
+    return return_code 
