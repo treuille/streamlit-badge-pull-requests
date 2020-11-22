f48957c5812d49017443435341a8a1aa5644e2dd
Adrien Treuille <adrien.g.treuille@gmail.com>
2020-11-22 09:01:22 -0800

    Caching the app badge status computation

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,23 @@ from github.GithubException import UnknownObjectException
 # This is where we will store all the forked repositories
 FORK_BASE_PATH = 'forks'
 
-def get_config():
+class ForkAppError(Exception):
+    """Represents a mistake that could happen when trying to fork an app."""
+
+    def __init__(self, reason):
+        """Constructor."""
+        Exception.__init__(self, reason)
+        self.reason = reason
+
+class ConfigOptions:
     """Returns all the config information to run the app."""
 
-    # Parse and return the information
-    access_token = st.sidebar.text_input("Github access token", type="password")
-    github = streamlit_github.from_access_token(access_token)
-    return github
+    def __init__(self) -> None:
+        """Adds UI elements which give us some config information."""
+        self.access_token = st.sidebar.text_input("Github access token", type="password")
+        self.auto_expand = st.sidebar.checkbox('Auto-expand app display')
+        self.auto_process_apps = st.sidebar.checkbox("Auto-process apps")
+        self.show_readmes = st.sidebar.checkbox("Show readme contents")
 
 @st.cache
 def get_s4a_apps() -> pd.DataFrame:
@@ -42,31 +52,18 @@ def filter_apps(apps: pd.DataFrame) -> pd.DataFrame:
 
     return selected_apps
 
-class ForkAppError(Exception):
-    """Represents a mistake that could happen when trying to fork an app."""
-
-    def __init__(self, reason):
-        """Constructor."""
-        Exception.__init__(self, reason)
-        self.reason = reason
-
-def parse_s4a_apps(apps: pd.DataFrame, github: GithubMainClass.Github):
+@st.cache(hash_funcs=streamlit_github.GITHUB_HASH_FUNCS, persist=True,
+            suppress_st_warning=True, ttl=(60 * 60 * 6)) 
+def compute_app_status(apps: pd.DataFrame, config: ConfigOptions, github: GithubMainClass.Github):
     # Whether to tunrn app details on by default.
-    auto_expand = st.sidebar.checkbox('Auto-expand app display')
-    auto_process_apps = st.sidebar.checkbox("Auto-process apps")
-    show_readmes = st.sidebar.checkbox("Show readme contents")
-
-    # Don't do anything until the use clicks this button.
-    if not (auto_process_apps or st.button('Process apps')):
-        return
 
     st.write('## Output')
 
     # These are the additional columns which we're going to add
     status_column = []
 
-    for i, app in enumerate(apps.itertuples()):
-        with st.beta_expander(app.app_url, expanded=auto_expand):
+    for app in apps.itertuples():
+        with st.beta_expander(app.app_url, expanded=config.auto_expand):
             try:
                 st.write(app)
                 
@@ -95,7 +92,7 @@ def parse_s4a_apps(apps: pd.DataFrame, github: GithubMainClass.Github):
                 if readme is None:
                     raise ForkAppError("Readme does not exist")
 
-                if show_readmes:
+                if config.show_readmes:
                     readme_contents = readme.decoded_content.decode('utf-8')
                     st.beta_columns((1, 20))[1].text(readme_contents)
 
@@ -114,11 +111,10 @@ def parse_s4a_apps(apps: pd.DataFrame, github: GithubMainClass.Github):
             status_column.append(app_status)
         
     # Assign these new columns to the app DataFrame.
-    apps['status'] = status_column
-
-    st.write("### Processed apps", apps)
-
-
+    st.write("Apps before:", type(apps), type(apps))
+    apps = apps.assign(status=status_column)
+    st.write("Apps after:", type(apps))
+    return apps
 
     # Print some results
     # apps.to_csv('out.csv')
@@ -126,54 +122,32 @@ def parse_s4a_apps(apps: pd.DataFrame, github: GithubMainClass.Github):
 
 def main():
     """Execution starts here."""
+    # These are all the options the user can set
+    config = ConfigOptions()
+
     # Get a github object from the user's authentication token
-    github = get_config()
+    github = streamlit_github.from_access_token(config.access_token)
 
     # Get the app dataframe
     apps = get_s4a_apps()
     apps = filter_apps(apps)
-    parse_s4a_apps(apps, github)
+        
+    # Don't do anything until the use clicks this button.
+    if not (config.auto_process_apps or st.button('Process apps')):
+        return
 
-#     # Test out content_file_from_app_url()
-#     app_url = "https://share.streamlit.io/shivampurbia/tweety-sentiment-analyis-streamlit-app/main/Tweety.py"
-#     f"**app_url:** `{app_url}`"
-#     coords = streamlit_github.GithubCoords.from_app_url(app_url)
-#     f"**coords:**", coords
-#     content_file = streamlit_github.get_contents(github, coords)
-#     st.write(dir(content_file))
-#     f"**content_file:**", content_file
-#     repo = content_file.repository
-#     f"**repo**", repo, repo.git_url
-#     st.write(dir(repo))
-#     st.code(readme_contents)
-#     'repo_has_badge', streamlit_github.BADGE_URL in readme_contents
-#     # st.code(content_file.
+    st.write("Filtered apps:", type(apps))
+    apps = compute_app_status(apps, config, github)
+    st.write("Computed apps:", type(apps))
 
-#     # Test out content_file_from_app_url()
-#     github_url = r"https://github.com/tester-burner/test1/blob/main/README.md"
-#     f'**github_url** `{github_url}`'
-#     coords = streamlit_github.GithubCoords.from_github_url(github_url)
-#     content_file = coords.get_contents(github)
-#     st.code(content_file.decoded_content.decode('utf-8'), language='markdown')
-#     
+    st.write("### Processed apps")
+    st.write(apps)
+
 #     if st.button('Fork the repo'):
 #         repo = coords.get_repo(github)
 #         'repo', repo
 #         streamlit_github.fork_and_clone_repo(repo, FORK_BASE_PATH)
 
-  
 # Start execution at the main() function 
 if __name__ == '__main__':
-    # Get the app dataframe
-    apps = get_s4a_apps()
-    # Get the app dataframe
-    apps = get_s4a_apps()
-    # Get the app dataframe
-    apps = get_s4a_apps()
-    # Get the app dataframe
-    apps = get_s4a_apps()
-    # Get the app dataframe
-    apps = get_s4a_apps()
-    # Get the app dataframe
-    apps = get_s4a_apps()
     main()
