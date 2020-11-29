8c9eb67966a865dad6589a76d980b04a6f9e456d
Adrien Treuille <adrien.g.treuille@gmail.com>
2020-11-28 20:50:57 -0800

    Adding a new script to clean up all my misspelled PRs

---
+++ clean_up_prs.py
@@ -0,0 +1,13 @@
+"""This is a second script which I'm creating to clean up every PR
+which had a misspelled title.
+
+Todo:
+
+- Create a list of all of the repos which we forked.
+- Create a template which performs actions and then captures errors
+  which can be displayed layer
+"""
+
+import streamlit as st
+
+st.write("Clean up PRs.")
