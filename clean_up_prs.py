5ed65ee40a88d1bc3f3f58d2567e05afddbcc51e
Adrien Treuille <adrien.g.treuille@gmail.com>
2020-11-30 21:35:47 -0800

    Starting next-gen version with the GitHubBot replacing streamlit_github

--- clean_up_prs.py
+++ clean_up_prs.py
@@ -6,8 +6,19 @@ Todo:
 - Create a list of all of the repos which we forked.
 - Create a template which performs actions and then captures errors
   which can be displayed layer
+
+Overview of pieces:
+
+- GithubCache
+    - map(input, func): map a function over a set and store the errors
+    - 
 """
 
 import streamlit as st
 
 st.write("Clean up PRs.")
+
+def double(x: int) -> int:
+    return x * 2
+
+st.write("double", double(10))
